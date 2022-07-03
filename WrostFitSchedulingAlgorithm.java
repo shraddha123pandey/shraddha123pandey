98f97af2f8511bcfc441ff8f029bd340b3bfc471
Shraddha Pandey <85031120+shraddha123pandey@users.noreply.github.com>
2022-07-03 09:01:01 +0530

    Add files via upload

---
+++ WrostFitSchedulingAlgorithm.java
@@ -0,0 +1,14 @@
+package OsPractical;
+
+
+/**
+ * 
+ * 1.) Input memory blocks and processes with sizes.
+ * 2.) Initialize all memory blocks as free.
+ * 3.) Start by picking
+ *
+ */
+
+public class WrostFitSchedulingAlgorithm {
+
+}
